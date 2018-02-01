9adee36588f59eb5c02cd5eb98684e53286a8723
durkesh <durkeshsr@gmail.com>
2018-02-01 15:25:06 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,10 @@
+def main():
+    x=input()
+    list=['a','e','i','o','u']
+    if x in list:
+        y="vowel"
+    else:
+        y="consonant"
+    print(y)
+if __name__ == '__main__':
+    main()
